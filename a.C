0e4a75188e449a736849f9cc60fa8e3055ded205
banmosh <emy.nobrega@gmail.com>
2021-08-10 14:54:36 -0300

    BUG REPORT: ~ decrypted as \n

--- a.C
+++ a.C
@@ -4,24 +4,25 @@
 
 using namespace std;
 
-char UPPER_FIRST_CHAR = 'A';
-char UPPER_LAST_CHAR = 'Z';
-string DATA, KEY, ENCRYPTED;
+char UPPER_FIRST_CHAR = 9; // space
+char UPPER_LAST_CHAR = 122; // 'z'
 
-string encrypt(string lddata, string lkkey){
-    int lddataSize, lkkeySize, charSum, i, k;
-    string enc = lddata;
-    lddataSize = lddata.size(); lkkeySize = lkkey.size();
+// !FIXME : BOTH INPUTS MUST BE UPPERCASE to work
+string encrypt(string data, string key){
+    int dataSize, keySize, charSum, i, k;
+    string enc = data;
+    dataSize = data.size(); keySize = key.size();
 
-    for (i = 0, k = 0; i < lddataSize; i++)
+    for (i = 0, k = 0; i < dataSize; i++)
     {
-        charSum = lddata[i] + lkkey[k] - UPPER_FIRST_CHAR; // BOTH INPUTS MUST BE UPPERCASE to work
+        charSum = data[i] + key[k] - UPPER_FIRST_CHAR; 
         if(charSum > static_cast<int>(UPPER_LAST_CHAR)) // keep from trespassing alphabet boundaries
-            charSum = UPPER_FIRST_CHAR + (charSum  - UPPER_LAST_CHAR) - 1;
+            charSum += UPPER_FIRST_CHAR - UPPER_LAST_CHAR - 1;
+            //charSum = UPPER_FIRST_CHAR + (charSum  - UPPER_LAST_CHAR) - 1;
         
         enc[i] = static_cast<char>(charSum);
         k++;
-        if(k >= lkkeySize)
+        if(k >= keySize)
             k = 0;
     }
 
@@ -29,14 +30,50 @@ string encrypt(string lddata, string lkkey){
     
 }
 
+// data[i] =  charSum - key[k] + UPPER_FIRST_CHAR; <-  charSum = data[i] + key[k] - UPPER_FIRST_CHAR;
+// ENC[I] =  data[i] + key[k]  - UPPER_LAST_CHAR - 1 -> data[i] = - key[k]  + UPPER_LAST_CHAR - enc[i] + 1
+//  L + Y - Z - 1 = J -> L = J - Y + 1
+// !FIXME : BOTH INPUTS MUST BE UPPERCASE to work
+string decrypt(string encrypted, string key){
+    int dataSize, keySize, charSum, i, k;
+    string dec = encrypted;
+    dataSize = encrypted.size(); keySize = key.size();
+
+    for (i = 0, k = 0; i < dataSize; i++)
+    {
+        charSum = encrypted[i] - key[k] + UPPER_FIRST_CHAR;               
+        if(charSum < static_cast<int>(UPPER_FIRST_CHAR))                 // keep from trespassing alphabet boundaries
+            charSum =  encrypted[i] - key[k]  + UPPER_LAST_CHAR + 1; //  L + Y - Z - 1 = J -> L = J - Y + 1
+        dec[i] = static_cast<char>(charSum);
+        k++;
+        if(k >= keySize)
+            k = 0;
+    }
+
+    return dec;
+    
+}
+
 int main(){
-    cout << static_cast<char>('L' + 'Y' - 'A') << endl; // what i needed
-   /*  cout << static_cast<char>('d' + 'd' - 'a') << endl; // what i needed
-    cout << ('a' + 'b') << endl;
-    cout << ('a' + 0) << endl ; */
+/*     cout << static_cast<char>('L' + 'Y' - 'A') << endl; // what i needed
+    cout << static_cast<char>('L' + 'Y' - 'Z' - 1) << endl; // what i needed
+
+    cout << encrypt("p \r <-para", "asZ") << endl; //rijvs
+    cout << decrypt( encrypt("p \n <-para", "asZ"), "KEY") << endl; //rijvs */
+    cout << decrypt( encrypt("Hel>~lO Wo\nrlD", "KEY"), "KEY") << endl; //rijvs // FIXME: TERMINAL DECRYPTS '~' AS '\n'
 
     cout << encrypt("HELLO", "KEY") << endl; //rijvs
+    cout << decrypt( encrypt("HELLO", "KEY"), "KEY") << endl; //rijvs
+
+    cout << encrypt("HELLO WORLD", "KEY") << endl; //rijvs
+    cout << decrypt( encrypt("HELLO WORLD", "KEY"), "KEY") << endl; //rijvs
+
+    cout << encrypt("F123rom there, we just need to split it into 6 columns (I mean in Python it would be tab  paragraph \n r \r what ", "KEY") << endl; //rijvs
+    cout << decrypt( encrypt("F123rom there, we just need to split it into 6 columns (I mean in Python it would be tab     paragraph \n r \r what ", "KEY"), "KEY") << endl; //rijvs
+
     cout << encrypt("HELLO", "SDBNO") << endl; //zhmyc
-    cout << encrypt("HELLOSDKFJHG", "SDBNO") << endl; //zhmyckglsxzj
+    cout << decrypt( encrypt("HELLO", "SDBNO"), "SDBNO") << endl; //zhmyc
+    cout << encrypt("HELLOSDKFJHG", "SDBNO") << endl; //zhmyckglsxzj 
+    cout << decrypt( encrypt("HELLOSDKFJHG", "SDBNO"), "SDBNO") << endl; //zhmyckglsxzj 
   return 0;
 }
