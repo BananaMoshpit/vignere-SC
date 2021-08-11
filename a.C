8f67a13e42f6008702bab4ceff063300c9d7885d
banmosh <emy.nobrega@gmail.com>
2021-08-11 09:33:41 -0300

    fixed get frequencies: only add a letter once

--- a.C
+++ a.C
@@ -2,9 +2,10 @@
 
 using namespace std;
 
-char FIRST_CHAR = 32; // space
-char LAST_CHAR = 122; // 'z'
-ofstream out("output.txt");
+char FIRST_CHAR = 30; // space
+char LAST_CHAR = 221; // 'z'
+
+//vector<pair<int,double,char> > vt = {(1,0.1,'a'),(2,4.2,'b')};
 
 char encrypt(char data, char key){
     char enc = data + key - FIRST_CHAR; 
@@ -58,8 +59,8 @@ string vigenere(string opt, string data, string key){
     
 }
 // FIXME: NOT N-SORTED: RECEIVES N-SORTED GROUPS
-vector<tuple<char,float>> get_frequency (string nGroup){
-    vector<tuple<char,float>> frequencies;
+vector<pair<char,float>> get_frequency (string nGroup){
+    vector<pair<char,float>> frequencies;
     float nGroupSize, count;
     char aux;
     nGroupSize = nGroup.size(); count = 0;
@@ -69,8 +70,8 @@ vector<tuple<char,float>> get_frequency (string nGroup){
     {
         aux = static_cast<char>(nGroup[i]);
         count = nGroup.find_last_of(aux) - nGroup.find_first_of(aux) + 1;
-        frequencies.push_back(make_tuple(aux, count/nGroupSize));
-        i += count ;
+        frequencies.push_back(make_pair(aux, count/nGroupSize));
+        i += count - 1;
     }
     
     return frequencies;
@@ -84,70 +85,28 @@ char find_key(char data, char enc){
   return static_cast<char>(key); 
 }
 
-bool tuple_sort_sec(const tuple<char , float>& a,
-               const tuple<char , float>& b)
-{
-    return (get<1>(a) > get<1>(b));
-}
-
-string break_vigenere(int opt, string data, int keySize, string reKey){
-    vector<tuple<char,float>> nFrequency;
-    vector<vector<tuple<char,float>>> allGroupsFrequencies;
-    char hlp;
-    string nGroup, key;
+void break_vigenere(int opt, string data, int keySize){
+    vector<pair<char,float>> nFrequency;
+    vector<vector<pair<char,float>>> allGroupsFrequencies;
+    string nGroup;;
     for (int keyN = 1; keyN <= keySize; keyN++)
     {
         nGroup.clear();
         for(int i = keyN - 1; i < data.size(); i += keySize)
         {
-            nGroup += static_cast<int>(data[i]);
+            nGroup += data[i];
         }
-        out << nGroup << " ";
+        cout << nGroup << " ";
         nFrequency = get_frequency(nGroup);
-        switch (keyN)
-        {
-        case 1:
-        hlp = vigenere("decrypt", static_cast<string>(get<0>(nFrequency[0])),"k")[0];
-            key += find_key(hlp ,'get<0>(nFrequency[0]));
-            break;
-                case 2:
-                hlp = vigenere("decrypt", get<0>(nFrequency[0],"e"))[0];
-            key += find_key(hlp ,'get<0>(nFrequency[0]));
-            break;
-                case 3:
-                hlp = vigenere("decrypt", get<0>(nFrequency[0],"y"))[0];
-            key += find_key(hlp ,'get<0>(nFrequency[0]));
-            break;
-        
-        default:
-        key += "!!";
-            break;
-        }
-/*         sort(nFrequency.begin(), nFrequency.end(), tuple_sort_sec);
-        key += find_key('e', get<0>(nFrequency[0])); */
     }
     
-    cout << "\n\n !!! KEYKEY >> "<< key << "\n\n " << endl;
-    return reKey;
-}
-
-void find_shift(vector<tuple<char,float>> data){
-    
+    cout << data << endl;
 }
 
 int main(){
-    string aux, key;
+    string aux;
 
-    
-
-    aux = "radarZmonkeyZsomeoneZissueZcoachZbrainZrareZcautionZcertainZwhaleZmatterZtoolZvisitZgirlZstateZflockZmerryZmushroomZtattooZauditZjunkZsirenZvirusZevidenceZjoinZbenefitZlabZgasZmethodZpenaltyZdebateZfanZcauseZpeaceZstoryZneitherZvolcanoZtrafficZbringZmuchZsectionZfenceZarmedZfeeZvesselZdwarfZearthZtwelveZbirthZfingerZmaterialZomitZacousticZmonsterZvolcanoZsilentZinnocentZdollZforumZloanZswallowZreviewZchurnZgymZoceanZfameZarmorZoldZquizZpumpkinZcountryZtellZfacultyZdemandZweatherZwaterZunfairZrewardZsituateZtorchZbraveZgunZshipZorangeZfatigueZdropZbubbleZbreadZvoiceZmedalZexplainZvillageZnobleZhugeZarrowZcardarrowZtorchZvendorZweirdZquizZneitherZsituateZsuchZsystemZravenZdieselZtextZtrayZdifferZamongZsuspectZaskZbargainZbundleZsilentZriskZjustZbeautyZembarkZpulseZpoleZincreaseZtowardZfuelZkiteZjeansZcheapZeffortZevolveZchampionZmailZspellZcardZcarryZclusterZhalfZunknownZtackleZclumpZimmuneZwishZwolfZbelow";
-    aux = "not very enticingly authoritative";
-    aux = vigenere("encrypt", aux, "hello");
-    key = break_vigenere(0, aux, 5, "hello");
-    cout << vigenere("decrypt", aux, key) << endl;
-    cout << "\n"<< vigenere("decrypt", aux, "hello") << endl;
-
-   /*  cout << find_key( 'B',encrypt('B', 215)) << endl;
+    cout << find_key( 'B',encrypt('B', 'C')) << endl;
     cout << find_key( '!',encrypt('!', '@')) << endl;
     cout << find_key( 'z',encrypt('z', '-')) << endl;
 
@@ -159,12 +118,11 @@ int main(){
 
     get_frequency("w1rrr3aaaa4");
     break_vigenere(0, "azbycx", 2);
-    break_vigenere(0, "abcdefabcdefabcdefabcdeff1", 6); */
-    out.close();
+    break_vigenere(0, "abcdefabcdefabcdefabcdeff1", 6);
   return 0;
 }
 
-/* BUG REPORT : FIXED, :: INPUT CHARA OVERFLOW-- MUST PASS A\SCII VAL IE 215 INSTREAD OF ×
+/* BUG REPORT 
 char LAST_CHAR = 221+;
     cout << find_key( 'B',encrypt('B', 'C')) << endl;
     cout << find_key( '!',encrypt('!', '@')) << endl;
